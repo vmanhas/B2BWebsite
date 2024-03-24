313c46c0e047c8ebdd2c776f39d1c146b72e7f8a
vmanhas <vishwajeetmanhas5091@gmail.com>
2024-03-24 12:18:35 +0530

    established mongdb connection

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-mongoose.connect("mongodb://localhost:27017/B2B");
+mongoose.connect(process.env.URI);
 app.use(cors());
 
 const ProductSchema = new mongoose.Schema({
@@ -13,7 +14,7 @@ const ProductSchema = new mongoose.Schema({
   image: String
 });
 
-const ProductModel = mongoose.model("Product", ProductSchema);
+const ProductModel = mongoose.model("products", ProductSchema);
 
 app.get('/api', async (req, res) => {
   try {
@@ -23,7 +24,6 @@ app.get('/api', async (req, res) => {
     console.error("Error getting products:", error);
     res.status(500).json({ message: "Error retrieving products" });
   }  
-  res.json(products);
 });
 
 app.listen(5000, () => console.log('Backend listening on port 5000'));
